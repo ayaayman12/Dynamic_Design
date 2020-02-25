a1697e0e86f1db65f30884d2992297e0bdb76576
ayaayman12 <39214939+ayaayman12@users.noreply.github.com>
2020-02-25 21:25:42 +0200

    commented

--- Dynamic Design/SERVICE/TMU/TMU.h
+++ Dynamic Design/SERVICE/TMU/TMU.h
@@ -9,24 +9,69 @@
 #ifndef TMU_H_
 #define TMU_H_
 
+/* Inclusions */
+#include "../../MCAL/timers.h"
 #include "../std_types.h"
 #include "TMU_Cfg.h"
 #include "TMU_PbCfg.h"
-#include "../../MCAL/timers.h"
 #include "../Errors.h"
 #include "../ERROR/ERROR-STORE.h"
 
-
-
+/* Constants */
 #define ONE_SHOT 0 
 #define PERIODIC 1
 
- ERROR_STATUS TMU_Init (const str_TMU_InitConfig_t * pstr_TMU_ConfigPtr );
+/* function declarations*/
 
- ERROR_STATUS TMU_Dispatch(void);
 
- ERROR_STATUS TMU_Stop(uint8_t Task_Id, void(* Ptr_Func)(void));
+/******************************************************************************
+* Function        : ERROR_STATUS TMU_Init (const str_TMU_InitConfig_t * pstr_TMU_ConfigPtr );                    
+* Type            : public                                                                  
+* Input parameters: TMUCfg Structure (u8_Timer_Id, u8_Tmu_Res)              
+* Return type     : ERROR_STATUS                                                            
+*                                                                               
+* Description: initialize the TMU configuration, which initialize the Timer.
+*****************************************************************************/
+extern ERROR_STATUS TMU_Init (const str_TMU_InitConfig_t * pstr_TMU_ConfigPtr );
 
- ERROR_STATUS TMU_Start (uint16_t Time_Delay,uint8_t Task_Id, uint8_t Repeat, void(* Ptr_Func)(void));
+/******************************************************************************
+* Function        : ERROR_STATUS TMU_Dispatch(void);              
+* Type            : public                                                                  
+* Input parameters: none   
+* Return type     : ERROR_STATUS                                                            
+*                                                                               
+* Description: check timing to serve the counsumer function.
+*****************************************************************************/
+extern ERROR_STATUS TMU_Dispatch(void);
+
+/******************************************************************************
+* Function        : ERROR_STATUS TMU_Stop(uint8_t Task_Id, void(* Ptr_Func)(void));                   
+* Type            : public                                                                  
+* Input parameters: Task_Id, void(* Ptr_Func)(void)
+* Return type     : ERROR_STATUS                                                            
+*                                                                               
+* Description: stop TMU for a specific task
+*****************************************************************************/
+extern ERROR_STATUS TMU_Stop(uint8_t Task_Id, void(* Ptr_Func)(void));
+
+/******************************************************************************
+* Function        : ERROR_STATUS TMU_Start (uint16_t Time_Delay,uint8_t Task_Id, uint8_t Repeat, void(* Ptr_Func)(void));                 
+* Type            : public                                                                  
+* Input parameters: Time_Delay, Task_Id, Repeat, void(* Ptr_Func)(void))
+* Return type     : ERROR_STATUS                                                            
+*                                                                               
+* Description: start TMU
+*****************************************************************************/
+extern ERROR_STATUS TMU_Start (uint16_t Time_Delay,uint8_t Task_Id, uint8_t Repeat, void(* Ptr_Func)(void));
+
+/******************************************************************************
+* Function        : ERROR_STATUS TMU_DeInit (void);               
+* Type            : public                                                                  
+* Input parameters: none
+* Return type     : ERROR_STATUS                                                            
+*                                                                               
+* Description: start TMU
+*****************************************************************************/
+ extern ERROR_STATUS TMU_DeInit (void);
 
 #endif /* TMU_H_ */
